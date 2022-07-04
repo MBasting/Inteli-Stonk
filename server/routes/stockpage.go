04c84d2051e582a084f7228f8fd65a7ff27855f2
M.J.Basting <M.J.Basting@student.tudelft.nl>
2022-07-04 14:10:36 +0200

    add more options (outputsize, interval, daily)

--- server/routes/stockpage.go
+++ server/routes/stockpage.go
@@ -10,17 +10,34 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// Request to get Stock Info:  http://localhost:8080/stock?name=APL
+// Request to get Stock Info:  http://localhost:8080/stock?name=AAPL&outputsize=compact&interval=30min
+// Request data:
+// - name => Stock Symbol
+// - outputSize => Compact (last 100 points) or full (all)
+// - interval => 1min, 5min, 15min, 30min, 60min, or any other string for Daily
 func GetStockPage(c echo.Context) error {
 	log.Println(c.Request().URL)
 	stockName := c.QueryParam("name")
-	fmt.Println("Stock name request is:", stockName)
+	interval := c.QueryParam("interval")
+	output := c.QueryParam("outputsize")
 
 	// TODO: Either include this in the Context body or define it somewhere more centrally
-	const APIkey string = "...."
+	const APIkey string = "7LMSYRQ2DGS73TRF"
+	var function string = ""
+	var intervalf string = ""
 
-	requestURL := fmt.Sprintf("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=%s&interval=5min&apikey=%s", stockName, APIkey)
-	fmt.Println("RequestURL:    ", requestURL)
+	if !(strings.Contains(interval, "min")) {
+		function = "TIME_SERIES_DAILY"
+		intervalf = ""
+	} else {
+		function = "TIME_SERIES_INTRADAY"
+		intervalf = fmt.Sprintf("&interval=%s", interval)
+	}
+
+	fmt.Println("Stock request is:", stockName, function, interval, output)
+
+	requestURL := fmt.Sprintf("https://www.alphavantage.co/query?function=%s&symbol=%s&outputsize=%s%s&apikey=%s", function, stockName, output, intervalf, APIkey)
+	fmt.Println("RequestURL: ", requestURL)
 
 	req, error := http.Get(requestURL)
 	if error != nil {
